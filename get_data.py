606537e8073af21172db6eb3f32f7ca664a88b45
Yuliya <sirena_travel@mail.ru>
2022-10-18 11:26:51 +0300

    get_data

--- get_data.py
+++ get_data.py
@@ -1,46 +1,31 @@
-#2. Создать телефонный справочник с возможностью импорта и экспорта данных в нескольких форматах.
-from pprint import pprint
+# 2. Создать телефонный справочник с возможностью импорта и экспорта данных в нескольких форматах.
 
 def last_names():
     last_names = []
+    names = []
+    tels = []
+    des = []
     while True:
         last_name = input("Введите фамилию: ")
         if last_name == 'end':
-            return last_names
-        last_names.append(last_name)
-
-def get_name():
-    names = []
-    while True:
+            break
         name = input("Введите имя: ")
-        if name == 'end':
-            return names
-        names.append(name)
-
-def telephone():
-    tels = []
-    while True:
         tel = input("Введите номер телефона: ")
-        if tel == 'end':
-            return tels
-        tels.append(tel)
-
-def info():
-    des = []
-    while True:
         info = input("Введите комментарий: ")
-        if info == 'end':
-            return des
+        last_names.append(last_name)
+        names.append(name)
+        tels.append(tel)
         des.append(info)
 
-
-def dic(last_names, names, tels, info):
-    pb={}
-    for i in range(1, len(last_names)+1):
-        key=i
-        pb[key]=[]
-        pb[key].append(last_names[i-1])
-        pb[key].append(names[i-1])
-        pb[key].append(tels[i-1])
-        pb[key].append(info[i-1])
+    pb = {}
+    for i in range(len(last_names)):
+        key = i + 1
+        pb[key] = []
+        pb[key].append(last_names[i])
+        pb[key].append(names[i])
+        pb[key].append(tels[i])
+        pb[key].append(des[i])
     return pb
+
+
+print(last_names())
